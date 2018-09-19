811eeb8daf7447926a726bab344dddab359da830
pstetz <pstetz@berkeley.edu>
2018-09-19 16:15:17 -0700

    simple button functions

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+    $( "#import_data" ).click(function() {
+			console.log("clicked import");
+		});
+
+		$( "#add_point" ).click(function() {
+			console.log("clicked add point");
+		});
+
+		$( "#clear_data" ).click(function() {
+			console.log("clicked clear");
+		});
+});
